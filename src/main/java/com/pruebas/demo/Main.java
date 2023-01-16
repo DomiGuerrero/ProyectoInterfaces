209aa5bc4e0da825469034339719ff9f48d7ce4c
dam <domingo98@hotmail.es>
2023-01-16 16:23:21 +0100

    Mi primer Commit

---
+++ src/main/java/com/pruebas/demo/Main.java
@@ -0,0 +1,84 @@
+package com.pruebas.demo;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import org.bson.Document;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Main extends Application {
+    final String FICHERO_CONFIGURACION ="settings.properties";
+    static Properties configuracion =  new Properties();
+    static MongoClient clienteMongo;
+    static MongoDatabase databaseMongo;
+
+    private void conectarBaseDatos(Properties config){
+        try {
+            clienteMongo =  MongoClients.create(config.getProperty("MONGO_URI"));
+            databaseMongo =clienteMongo.getDatabase(  config.getProperty("MONGODB_DATABASE"));
+        }catch (Exception ex){
+            Alert alerta =  new  Alert(Alert.AlertType.ERROR);
+            alerta.setHeaderText("Alerta de Error");
+            alerta.setContentText("Error: no se ha podido acceder a la base de datos");
+            alerta.show();
+        }
+
+
+    }
+    private void cargarConfiguracion(String fichero_configuracion) {
+        try{
+            InputStream input = this.getClass().getClassLoader().getResourceAsStream(fichero_configuracion);
+            configuracion.load(input);
+            System.out.println(configuracion.getProperty("MONGO_URI"));
+
+        } catch (IOException e) {
+            Alert alerta =  new  Alert(Alert.AlertType.ERROR);
+            alerta.setHeaderText("Alerta de Error");
+            alerta.setContentText("Error no se ha podido leer " + fichero_configuracion);
+            alerta.show();
+        }
+    }
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        cargarConfiguracion(FICHERO_CONFIGURACION);
+        conectarBaseDatos(configuracion);
+        Document pelicula =  new Document();
+        pelicula.append("nombre","Gran Torino")
+                .append("nombre_original", "Gran Torino")
+                .append("plot", "Walt Kowalski, un veterano de la guerra de Corea," +
+                        " es un obrero jubilado del sector del automóvil que ha enviudado recientemente. " +
+                        "Su máxima pasión es cuidar de su más preciado tesoro: un coche Gran Torino de 1972. " +
+                        "Es un hombre inflexible y cascarrabias, al que le cuesta trabajo asimilar los cambios " +
+                        "que se producen a su alrededor, " +
+                        "especialmente la llegada de multitud de inmigrantes asiáticos a su barrio. Sin embargo, " +
+                        "las circustancias harán que se vea obligado a " +
+                        "replantearse sus ideas.")
+                .append("Fecha_lanzamiento",2008);
+        System.out.println(databaseMongo.getCollection("Peliculas").insertOne(pelicula));
+
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Información de películas");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module com.pruebas.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.mongodb.driver.core;
+    requires org.mongodb.bson;
+    requires org.mongodb.driver.sync.client;
 
 
     opens com.pruebas.demo to javafx.fxml;
